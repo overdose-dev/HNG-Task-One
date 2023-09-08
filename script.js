10db1be7990c5f001404bb6f0c5e17357b9f43d6
Chinazaekpere <chinazauchenna@gmail.com>
2023-09-08 06:31:58 +0100

    added the script file for project

---
+++ script.js
@@ -0,0 +1,32 @@
+function updateClock() {
+  var weekDayElement = document.getElementById("main_weekday");
+  var timeElement = document.getElementById("mainUTCTime");
+
+  const currentDate = new Date();
+  const dayOfWeek = currentDate.getUTCDay();
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayName = daysOfWeek[dayOfWeek];
+  const hours = currentDate.getUTCHours().toString().padStart(2, "0");
+  const minutes = currentDate.getUTCMinutes().toString().padStart(2, "0");
+  const seconds = currentDate.getUTCSeconds().toString().padStart(2, "0");
+  const milliseconds = currentDate
+    .getUTCMilliseconds()
+    .toString()
+    .padStart(2, "0");
+  const currentTimeAndDay = `Current UTC Time: ${hours}:${minutes}:${seconds}.${milliseconds}, Day of the Week: ${dayName}`;
+
+  weekDayElement.innerHTML = dayName;
+  timeElement.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
