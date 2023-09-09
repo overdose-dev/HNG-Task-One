66c7f2c69e0800962b52038d71803c3dec236cd1
Chinazaekpere <chinazauchenna@gmail.com>
2023-09-09 20:19:08 +0100

    removed the time

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function updateClock() {
   var weekDayElement = document.getElementById("main_weekday");
   var UTCtimeElement = document.getElementById("currentUTCTime");
-  var timeElement = document.getElementById("Time");
 
   const currentDate = new Date();
   const dayOfWeek = currentDate.getUTCDay();
@@ -15,14 +14,10 @@ function updateClock() {
     "Saturday",
   ];
   const dayName = daysOfWeek[dayOfWeek];
-  const hours = currentDate.getUTCHours().toString().padStart(2, "0");
-  const minutes = currentDate.getUTCMinutes().toString().padStart(2, "0");
-  const seconds = currentDate.getUTCSeconds().toString().padStart(2, "0");
   const milliseconds = currentDate.getTime();
 
   weekDayElement.innerHTML = dayName;
   UTCtimeElement.innerHTML = `${milliseconds}`;
-  timeElement.innerHTML = `${hours}:${minutes}:${seconds}`;
 }
 
 setInterval(updateClock, 1000);
